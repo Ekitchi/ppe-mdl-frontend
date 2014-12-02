d114e916b787633457848aea31150a2bf671a0b2
Wal Low <anti.factions@gmail.com>
2014-12-02 19:10:14 +0100

    Formulaire update
    La fonction de submit ne se déclenche désormais seulement lorsque le
    formulaire est valide.
    Affichage des informations à envoyer dans la DB en console
    Affichage d'une alert si les mots de passe ne correspondent pas

--- js/controllers.js
+++ js/controllers.js
@@ -14,13 +14,37 @@ angular.module('mdl.controllers', [])
         */
         $scope.ConfirmRegister = function()
         {
-                if ($scope.RegisterIsValid == true) {
+        	console.log("Informations qui seront rentrées dans la DB");
+        	console.log($scope.inscriptionCtrl.pseudo);
+        	console.log($scope.inscriptionCtrl.nom);
+        	console.log($scope.inscriptionCtrl.prenom);
+        	console.log($scope.inscriptionCtrl.email);
+        	console.log($scope.inscriptionCtrl.password);
+        	console.log($scope.inscriptionCtrl.passwordConfirm);
+        	console.log($scope.inscriptionCtrl.date);
+        	console.log($scope.inscriptionCtrl.telephone);
+        	
+        	
+        	var mdp1 = $scope.inscriptionCtrl.password;
+        	var mdp2 = $scope.inscriptionCtrl.passwordConfirm;
+        	
+        	if (mdp1 === mdp2){
+        		console.log("Mot de passe: ok");
+        	}
+        	else{
+        		console.log("Mot de passe: ERROR");
+        		/*$scope.registerForm = $scope.registerForm.$invalid;*/
+        		alert('Les mots de passe ne correspondent pas');
+        	}
+        	
+        	
+               /* if ($scope.RegisterIsValid == true) {
                     $location.path('/ConfirmedRegister');
                 }
                 else {
                     console.log("Error in the inscription. Please review your form");
-                }
-        }
+                }*/
+        };
 
     }
     ]);

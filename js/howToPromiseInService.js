e255f2cf3d95a39c0864eec47c5e48461a93801f
William <yoshi93@gmail.com>
2014-12-03 14:49:19 +0100

    Added getHome, getListLeagues and getMap function. Only adding public getters for now.

--- js/howToPromiseInService.js
+++ js/howToPromiseInService.js
@@ -6,34 +6,42 @@ Please add $http and $q to your service factory dependencies.
 // This is to add before any of your service functions.
 
 function wrapped$httpPromise(httpCallConfig) {
-	            var deferred = $q.defer();
-	
-	            $http(httpCallConfig).
-	                success(function(body) {
-	                    deferred.resolve(body);
-	                }).
-	                error(function (data) {
-	                    deferred.reject(data);
-	                });
-	
-	            return deferred.promise;
-	        }
-            
+    var deferred = $q.defer();
+
+    $http(httpCallConfig).
+    success(function(body) {
+        deferred.resolve(body);
+    }).
+    error(function(data) {
+        deferred.reject(data);
+    });
+
+    return deferred.promise;
+}
+
 
 // You will need to make a return statement. This statement contains all your back request. There is one return, which includes you different function.
 
+// Those are the GET request.
 
 return {
-            	getDefaultUrl: function() {
-            		return wrapped$httpPromise({
-            			method: 'GET',
-            			url: 'webapi/url'
-            		});
-                    
-                getSomething: function() {
-                    return wrapped$httpPromise({
-                        method: 'GET',
-                        url: 'webapi/whatever'
-                    });
-                }
-            	},
+    getHome: function() {
+        return wrapped$httpPromise({
+            method: 'GET',
+            url: '/home/'
+        });
+    },
+
+    getListeLeagues: function() {
+        return wrapped$httpPromise({
+            method: 'GET',
+            url: '/leagues/'
+        });
+    },
+    getMap: function() {
+        return wrapped$httpPromise({
+            method: 'GET',
+            url: '/map/'
+        })
+    },
+};

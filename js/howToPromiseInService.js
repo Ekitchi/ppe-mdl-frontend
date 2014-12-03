c7d43c6ecefce7b41127d6fea4bf9cdd782656aa
William <yoshi93@gmail.com>
2014-12-03 11:00:42 +0100

    Added a 'howToPromiseInService.js'. Will be completed over time. Should teach you the basics of using promises concept for our project.

---
+++ js/howToPromiseInService.js
@@ -0,0 +1,39 @@
+/*
+This is the requiered components to bind our webapp to the REST Service provided by the other team.
+Please add $http and $q to your service factory dependencies.
+*/
+
+// This is to add before any of your service functions.
+
+function wrapped$httpPromise(httpCallConfig) {
+	            var deferred = $q.defer();
+	
+	            $http(httpCallConfig).
+	                success(function(body) {
+	                    deferred.resolve(body);
+	                }).
+	                error(function (data) {
+	                    deferred.reject(data);
+	                });
+	
+	            return deferred.promise;
+	        }
+            
+
+// You will need to make a return statement. This statement contains all your back request. There is one return, which includes you different function.
+
+
+return {
+            	getDefaultUrl: function() {
+            		return wrapped$httpPromise({
+            			method: 'GET',
+            			url: 'webapi/url'
+            		});
+                    
+                getSomething: function() {
+                    return wrapped$httpPromise({
+                        method: 'GET',
+                        url: 'webapi/whatever'
+                    });
+                }
+            	},

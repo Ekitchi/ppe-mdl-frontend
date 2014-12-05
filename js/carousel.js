ebd515bd4fb768649e4e2896f3b21d7e3e297ffc
Wal Low <anti.factions@gmail.com>
2014-12-05 21:04:49 +0100

    Owl carousel full working
    Voilà, on a un carousel qui fonctionne très bien

--- js/carousel.js
+++ js/carousel.js
@@ -8,11 +8,15 @@
 		items : 1,
 		loop : true,
 		autoplay : true,
-		autoplayTimeout : 3500,
+		autoplayTimeout : 4000,
 		autoplayHoverPause : true,
-		animationIn : true,
-		animationOut : true,
-		margin : 30
+		margin : 30,
+		nav: true,
+		navText: ['<i class="glyphicon glyphicon-chevron-left"></i>', '<i class="glyphicon glyphicon-chevron-right"></i>'],
+		smartSpeed: 900,
+		video: true,
+		navSpeed: 900,
+		animateOut:"fadeOut"
 	});
 
 })(jQuery); 

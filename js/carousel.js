8c320260d69aa6168d29d1b63c8fe456eefc8e4f
Wal Low <anti.factions@gmail.com>
2014-12-05 18:19:24 +0100

    ADD THIS FUCKING CAROUSEL
    Et si il vous plait pas, enfoncez-vous un concombre dans l'anus.

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+/**
+ * @author Alessio
+ */
+
+(function($) {
+
+	$('.carousel').owlCarousel({
+		items : 1,
+		loop : true,
+		autoplay : true,
+		autoplayTimeout : 2000,
+		autoplayHoverPause : true,
+		animationIn : true,
+		animationOut : true,
+		margin : 30
+	});
+
+})(jQuery); 

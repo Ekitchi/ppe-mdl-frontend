8f5a3e96d64808037cfadad7951d8d906019e03c
William <yoshi93@gmail.com>
2014-12-12 15:21:25 +0100

    Default partial displayed on / is now 'accueil'. Added accueil.html with sample goal of the page. Controller needs to be written and use ng-click to redirect the list items, for  purposes.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 angular.module('MDL', ['ngRoute', 'mdl.controllers', 'ui.bootstrap'])
-.config(['$routeProvider',
-    function($routeProvider) {
+    .config(['$routeProvider',
+        function($routeProvider) {
 
-        $routeProvider.when('/', {
-            templateUrl: 'partials/inscription.html',
-            controller: 'InscriptionController',
-            access: {
-                isFree: true
-            }
-        });
-        
-        $routeProvider.when('/connexion', {
-            templateUrl: 'partials/connexion.html',
-            controller: 'ConnexionController'
-        });
-        
-        $routeProvider.when('/league/:id', {
-            templateUrl: 'partials/league.html',
-            controller: 'LeagueController'
-        });
-        
-        $routeProvider.when('/league', {
-        	templateUrl: 'partials/add_league.html',
-        	controller: 'AddLeagueController'
-        });
+            $routeProvider.when('/', {
+                templateUrl: 'partials/accueil.html',
+                controller: 'AccueilController',
+                access: {
+                    isFree: true
+                }
+            });
+            $routeProvider.when('/inscription', {
+                templateUrl: 'partials/inscription.html',
+                controller: 'InscriptionController',
+            });
 
-    }
-]);
+            $routeProvider.when('/connexion', {
+                templateUrl: 'partials/connexion.html',
+                controller: 'ConnexionController'
+            });
+
+            $routeProvider.when('/league/:id', {
+                templateUrl: 'partials/league.html',
+                controller: 'LeagueController'
+            });
+
+            $routeProvider.when('/league', {
+                templateUrl: 'partials/add_league.html',
+                controller: 'AddLeagueController'
+            });
+
+        }
+    ]);

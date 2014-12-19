20201bbdb78f6ebe69f19268228f5167f4c826ec
Erwhann-Rouge <erwhann-rouge@outlook.com>
2014-12-19 17:33:20 +0100

    [Hotfix] Inscription fonctionnelle
    Attention, j'ai bidouillé l'url d'envoi

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
 angular.module('mdl.service', [])
 .constant('_', window._)
-.factory('MdlService', ['_', '$http', '$q', 
+.factory('MdlService', ['_', '$http', '$q',
 					function(_, $http, $q) {
-					
+
 
 
 	// This kinda initiate the whole promise thing. Don't touch it.
@@ -33,13 +33,14 @@ angular.module('mdl.service', [])
 		// Logging in console. Just to be sure about those fucking double quotes.
 		console.log(jsonObj);
 
+
 		//This is where the magic happens.
         return wrapped$httpPromise({
             method: 'POST',
             // Headers not working. Delete it to actually proceed the request. Or don't. I don't care. Fuck this shit. - Angry dev @5:15PM 19/12/2014
-            headers: {'Content-Type': charset=UTF-8},
-            url: 'http://localhost:8888/Y2/Symfony/web/app_dev.php/user/',
-            data: jsonObj
+            headers: {'Content-Type': "application/x-www-form-urlencoded"},
+            url: 'http://localhost/ppe/Symfony/web/app_dev.php/user/',
+            data: "data="+jsonObj
         	});
 		}
 	}

151ba809b769d11ec786291f07d4e5b70d1764cf
William <yoshi93@gmail.com>
2014-12-19 17:15:45 +0100

    Added some comments on service.js . Just in case.

--- js/service.js
+++ js/service.js
@@ -21,18 +21,22 @@ angular.module('mdl.service', [])
 
 	//Here are your multiple requests that will establish communication with the REST service.
 	return {
-
+	//We'll call this       with those params
 		postUser : function(firstNameValue, lastNameValue, emailValue, passwordValue) {
+		// Ugliest JSON maker I've ever seen
 		var obj = {};
 		obj.name = lastNameValue;
 		obj.first_name = firstNameValue;
 		obj.email = emailValue;
 		obj.password = passwordValue;
 		var jsonObj = JSON.stringify(obj);
+		// Logging in console. Just to be sure about those fucking double quotes.
 		console.log(jsonObj);
 
+		//This is where the magic happens.
         return wrapped$httpPromise({
             method: 'POST',
+            // Headers not working. Delete it to actually proceed the request. Or don't. I don't care. Fuck this shit. - Angry dev @5:15PM 19/12/2014
             headers: {'Content-Type': charset=UTF-8},
             url: 'http://localhost:8888/Y2/Symfony/web/app_dev.php/user/',
             data: jsonObj
